3b3c12c776718c64a0c41e183733be843a764041
duoduo <zerolin_0428@163.com>
2013-05-26 11:46:01 +0800

    new project storage_sys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StorageSys::Application.config.secret_token = 'dc0454fa1173599042e1406dce63991b2fd5a4614c7a87b899d743d9bb94875e337ccf8e29149b94f6d8760d1ed3d2357fb0726f12e9b8322b4b4fb771443321'
