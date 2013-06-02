8efb6689d55147a9369cb3d03a1c08a3bd91017d
duoduo <zerolin_0428@163.com>
2013-06-02 13:36:06 +0800

    one sql

--- db/original_data.sql
+++ db/original_data.sql
@@ -4,3 +4,5 @@ insert into storage.statuses (id,name,created_at,updated_at) values (2,"NOT NULL
 TRUNCATE table storage.competences;
 insert into storage.competences (id,name,ostatus_id,nstatus_id,created_at,updated_at) values (1,"add",1,2,"20130602122330","20130602122330");
 insert into storage.competences (id,name,ostatus_id,nstatus_id,created_at,updated_at) values (2,"delete",2,1,"20130602122330","20130602122330");
+truncate table storage.users;
+truncate table storage.cagos;
